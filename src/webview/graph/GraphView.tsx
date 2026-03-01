8c2479dfbe7ba7a5d604f90ed55338d81e9049e3
Daniel Amaral <d4nielamaral@gmail.com>
2026-03-01 19:35:46 +0100

    feat: add drop commit (#4)

--- src/webview/graph/GraphView.tsx
+++ src/webview/graph/GraphView.tsx
@@ -210,26 +210,31 @@ export function GraphView({ commits }: Props) {
                     style={{ display: 'block', left: singleMenu.x, top: singleMenu.y }}
                     onClick={(e) => e.stopPropagation()}
                 >
-                    <div className="context-menu-item" onClick={() => handleSingleAction('showCommitDetails')}>
-                        Show more details
+                    <div className="context-menu-item" onClick={() => handleSingleAction('editCommitMessage')}>
+                        Edit Commit Message
+                    </div>
+                    <div className="context-menu-item" onClick={() => handleSingleAction('cherryPick')}>
+                        Cherry Pick
                     </div>
                     <div className="context-menu-separator" />
                     <div className="context-menu-item" onClick={() => handleSingleAction('copyHash')}>
                         Copy Hash
                     </div>
-                    <div className="context-menu-separator" />
-                    <div className="context-menu-item" onClick={() => handleSingleAction('cherryPick')}>
-                        Cherry Pick
-                    </div>
                     <div className="context-menu-item" onClick={() => handleSingleAction('revertCommit')}>
                         Revert Commit
                     </div>
-                    <div className="context-menu-separator" />
-                    <div className="context-menu-item" onClick={() => handleSingleAction('editCommitMessage')}>
-                        Edit Commit Message
-                    </div>
                     <div className="context-menu-item" onClick={() => handleSingleAction('resetToCommit')}>
                         Reset to Commit
+                    </div>
+                    <div
+                        className="context-menu-item context-menu-item--danger"
+                        onClick={() => handleSingleAction('dropCommit')}
+                    >
+                        Drop Commit
+                    </div>
+                    <div className="context-menu-separator" />
+                    <div className="context-menu-item" onClick={() => handleSingleAction('showCommitDetails')}>
+                        Show more details
                     </div>
                 </div>
             )}
